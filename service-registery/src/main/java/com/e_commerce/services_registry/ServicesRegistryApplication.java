eb7404f5ca9be4c680f80f240e1ae75974a40f56
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-09-15 17:36:04 +0300

    upload registery-service

---
+++ service-registery/src/main/java/com/e_commerce/services_registry/ServicesRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.e_commerce.services_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServicesRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesRegistryApplication.class, args);
+	}
+
+}
