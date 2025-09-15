87603dffe05062b3db4b309eaf0e1a78e2211544
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-09-15 20:35:08 +0300

    fix error in inventory service

--- inventory-service/src/main/java/com/e_Commerce/inventory_service/models/Inventory.java
+++ inventory-service/src/main/java/com/e_Commerce/inventory_service/models/Inventory.java
@@ -7,18 +7,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "Inventories")
+@NoArgsConstructor
 public class Inventory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "product_id", updatable = false ,nullable = false)
+    @Column(name = "product_id", updatable = false ,nullable = false, unique = true)
     private Long productId;
 
     @Column(name = "quantity", updatable = false ,nullable = false)
