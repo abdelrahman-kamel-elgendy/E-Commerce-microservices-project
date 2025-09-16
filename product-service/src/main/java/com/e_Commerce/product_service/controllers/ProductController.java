a89c5cdfb891a5305e362da16351be545844442d
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-09-16 14:05:19 +0300

    update product controller

--- product-service/src/main/java/com/e_Commerce/product_service/controllers/ProductController.java
+++ product-service/src/main/java/com/e_Commerce/product_service/controllers/ProductController.java
@@ -1,7 +1,6 @@
 package com.e_Commerce.product_service.controllers;
 
 import java.util.List;
-import java.util.Locale.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
