9ceed7d9dbea0225bc75b15899a9901c7e7890b6
abdelrahman-kamell <abdelrahman.kamel@si-ware.com>
2025-09-16 13:23:12 +0300

    update user model

--- user-service/src/main/java/com/e_commerce/user_service/models/User.java
+++ user-service/src/main/java/com/e_commerce/user_service/models/User.java
@@ -8,15 +8,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
